d98e1590727624663bbc9690dced5f2c468dea5d
Anthony Kotoyan <kotoyan.anthony@gmail.com>
2024-10-19 00:55:48 -0700

    Add files via upload

---
+++ OBJ_Render/src/engine/Vector3f.java
@@ -0,0 +1,15 @@
+package engine;
+
+public class Vector3f {
+    public float x, y, z;
+
+    public Vector3f(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3f add(Vector3f other) {
+        return new Vector3f(this.x + other.x, this.y + other.y, this.z + other.z);
+    }
+}
